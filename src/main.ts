f81c76e88e0f427e7770140a96bac31f1afd52a9
Yukiya Okita <yukiya.okita.dev@gmail.com>
2023-02-07 00:37:57 +0900

    fix

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { Request } from 'express';
+import cookieParser, * as CookieParser from 'cookie-parser';
+import * as csurf from 'csurf';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.enableCors({
+    credentials: true,
+    origin: ['http://localhost:3000'],
+  });
+  // jwtをcookieベースで通信する、frontendから受け取ったcookieを解析するための設定
+  app.use(cookieParser());
   await app.listen(3005);
 }
 bootstrap();
